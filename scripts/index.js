9f04e2a7f51b1020c9171f6d62ca7fe4c0945f54
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-09-09 09:38:31 -0400

    Added Materialize and cards for testing UI.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,28 @@
+let content = 
+`
+<div class="col s12 m7">
+<div class="card horizontal">
+  <div class="card-image">
+    <img src="https://lorempixel.com/100/190/nature/6">
+  </div>
+  <div class="card-stacked">
+    <div class="card-content">
+      <p>I am a very simple card. I am good at containing small bits of information.</p>
+    </div>
+    <div class="card-action">
+      <a href="#">This is a link</a>
+    </div>
+  </div>
+</div>
+</div>
+`;
+
 let myLibrary = [];
+const bookContainer = document.querySelector(".book-container");
+const addBookButton = document.querySelector("#add-book-button");
+addBookButton.addEventListener("click", function(){
+  bookContainer.innerHTML += content;
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -15,4 +39,6 @@ function addBookToLibrary(title, author, pages, read) {
 function render() {
 
 }
+
+
 
